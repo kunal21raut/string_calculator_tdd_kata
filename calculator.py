208fafabd70a770e21182fd51af4237d283a14a5
kunal21raut <kunaldraut2100@gmail.com>
2025-04-14 13:11:33 +0530

    added the correct filename such that my tests should run

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
+
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+    
+    if not numbers:
+        return 0
+
+def add(numbers: str) -> int:
+    if not numbers:
+        return 0
+    return int(numbers)
--- test_calculator.py
+++ test_calculator.py
@@ -3,5 +3,9 @@ from calculator import add
 def test_empty_string_returns_0():
     assert add("") == 0
 
+def test_single_number():
+    assert add("5") == 5
+
+
 
 
