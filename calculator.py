b9f47cb30b9ac46c0a3f78407116652f0f6cbecc
kunal21raut <kunaldraut2100@gmail.com>
2025-04-14 13:28:38 +0530

    after adjusting the  code for the different delimiters support still test failing

--- calculator.py
+++ calculator.py
@@ -1,7 +1,15 @@
 
 def add(numbers: str) -> int:
     total = 0
-    numbers = numbers.replace("\n", ",")
+    
+    delimiter = ","
+    if numbers.startswith("//"):
+        parts = numbers.split("\n", 1)
+        delimiter = parts[0][2:]
+        numbers = parts[1]
+
+    numbers = numbers.replace("\n", delimiter)
+    
     if numbers:
         for n in numbers.split(","):
             total += int(n)
