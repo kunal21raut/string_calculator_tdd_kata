c5157c52bbd5390384b12098dacd7add108593b9
kunal21raut <kunaldraut2100@gmail.com>
2025-04-14 13:21:04 +0530

    fixed failing test and added new test which allow newline  \n as a delimiter but not handled that within the code so one test will fail

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,27 @@
 from calculator import add
 
+#Empty string returns 0
 def test_empty_string_returns_0():
     assert add("") == 0
 
+# One number returns the number
 def test_single_number():
     assert add("5") == 5
 
+# Two comma-separated numbers
 def test_two_numbers():
-    assert add("1,9") == 1
+    assert add("1,9") == 10
+
+#Handle any amount of numbers
+def test_multiple_numbers():
+    assert add("192,3,5") == 200
+
+
+
+# Allow newline \n as a delimiter 
+def test_newline_as_delimiter():
+    assert add("1\n2,3") == 6
+
 
 
 
